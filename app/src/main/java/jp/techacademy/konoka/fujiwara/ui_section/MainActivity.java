a9d5efa7cd52f5fe7ac0f39f2ef9aaa35f60c1f9
konoka <ririmaru2003@gmail.com>
2018-12-02 14:26:59 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/konoka/fujiwara/ui_section/MainActivity.java
@@ -0,0 +1,16 @@
+package jp.techacademy.konoka.fujiwara.ui_section;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
